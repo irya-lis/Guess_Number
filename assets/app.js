610bad57536523f4e7372dcc89bd31c47400934d
Irina Rakova <lis.ovskaya1119@gmail.com>
2022-10-31 20:30:04 +0300

    Initial commit

---
+++ assets/app.js
@@ -0,0 +1,63 @@
+let secretNum = Math.ceil(Math.random() * 10);
+let tries = 5;
+
+function guessNum(num) {
+    if (tries) {
+
+        if (num === secretNum) {
+            tries = 0;
+            console.log(`Ваше число: ${num}  - Число угадано! Вы победили!!!`);
+            return console.log('Игра окончена.');
+        }
+
+        if (num !== secretNum) {
+            tries--;
+            console.log(`Ваше число: ${num} - Попробуйте еще раз!`);
+
+            if (num > secretNum) {
+                console.log('Загаланное число меньше');
+            } else if (num < secretNum) {
+                console.log('Загаданное число больше');
+            }
+        }
+
+        if (tries === 0) {
+            return console.log('Вы исчерпали попытки!!! Игра окончена.');
+        }
+    }
+
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
